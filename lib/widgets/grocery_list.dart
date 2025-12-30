c8d43d089c08fcb71ac44d011d870d3449c8d87e
johnatan <johnatanv91@gmail.com>
2025-12-30 16:42:39 -0500

    grocery_list widget changed to stateful one

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list_app/data/dummy_items.dart';
 
-class GroceryList extends StatelessWidget{
+class GroceryList extends StatefulWidget{
   const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  void _addItem() {
+    Navigator.of(context);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +20,12 @@ class GroceryList extends StatelessWidget{
         title: const Text(
           'Your Groceries'
         ),
+        actions: [
+          IconButton(
+            onPressed: _addItem, 
+            icon: const Icon(Icons.add),
+          ),
+        ],
       ),
       body: ListView.builder(
         itemCount: groceryItems.length,
---
+++ lib/widgets/new_item.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class NewItem extends StatefulWidget {
+  const NewItem({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _NewItemState();
+  }
+}
+
+class _NewItemState extends State<NewItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Add a new item',
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsetsGeometry.all(12.0),
+        child: Text('The Form'),
+      ),
+    );
+  }
+}
