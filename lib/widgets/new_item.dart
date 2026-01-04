5122be0c93a3f80eaa35dce38250532ed550cfe1
johnatan <johnatanv91@gmail.com>
2026-01-04 18:21:04 -0500

    renderbox correction for form

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -32,33 +32,38 @@ class _NewItemState extends State<NewItem> {
                 },
               ),
               Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      label: Text('Quantity')
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        label: Text('Quantity')
+                      ),
+                      initialValue: '1',
                     ),
-                    initialValue: '1',
                   ),
                   const SizedBox(width: 8.0,),
-                  DropdownButtonFormField(
-                    items: [
-                      for (final category in categories.entries)
-                        DropdownMenuItem(
-                          value: category.value,
-                          child: Row(
-                            children: [
-                              Container(
-                                width: 16,
-                                height: 16,
-                                color: category.value.color ,
-                              ),
-                              const SizedBox(width: 6.0,),
-                              Text(category.value.title)
-                            ],
-                          )
-                        ),
-                    ], 
-                    onChanged: (value) {}
+                  Expanded(
+                    child: DropdownButtonFormField(
+                      items: [
+                        for (final category in categories.entries)
+                          DropdownMenuItem(
+                            value: category.value,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 16,
+                                  height: 16,
+                                  color: category.value.color ,
+                                ),
+                                const SizedBox(width: 6.0,),
+                                Text(category.value.title)
+                              ],
+                            )
+                          ),
+                      ], 
+                      onChanged: (value) {}
+                    ),
                   ),
                 ],
               )
