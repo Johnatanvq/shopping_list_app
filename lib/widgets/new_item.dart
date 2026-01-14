8b7f0a8ec9029c0351f8ae1ddd1c9b726929e39b
johnatan <johnatanv91@gmail.com>
2026-01-14 17:24:15 -0500

    getting form access via global key

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -9,6 +9,12 @@ class NewItem extends StatefulWidget {
   }
 }
 class _NewItemState extends State<NewItem> {
+  final _formKey = GlobalKey<FormState>();
+
+  void _saveItem() {
+    _formKey.currentState!.validate();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +26,7 @@ class _NewItemState extends State<NewItem> {
       body: Padding(
         padding: const EdgeInsetsGeometry.all(12.0),
         child: Form(
+          key: _formKey,
           child: Column(
             children: [
               TextFormField(
@@ -83,11 +90,13 @@ class _NewItemState extends State<NewItem> {
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   TextButton(
-                    onPressed: () {}, 
+                    onPressed: () {
+                      _formKey.currentState!.reset();
+                    }, 
                     child: const Text('Reset')
                   ),
                   ElevatedButton(
-                    onPressed: () {}, 
+                    onPressed: _saveItem, 
                     child: const Text('Add Item')
                   ),
                 ],
