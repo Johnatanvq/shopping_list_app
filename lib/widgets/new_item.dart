f2182c1bee702d2a2f83f1ca0ebb99cde6bb22b7
johnatan <johnatanv91@gmail.com>
2026-01-17 11:59:48 -0500

    working with the request & waiting for the response

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -17,11 +17,11 @@ class _NewItemState extends State<NewItem> {
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
 
-  void _saveItem() {
+  void _saveItem() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
       final url = Uri.https('shopping-list-app-124ad-default-rtdb.firebaseio.com', 'shopping-list.json');
-      http.post(
+      final response = await http.post(
         url, 
         headers: {
           'Content-Type' : 'application/json',
@@ -32,6 +32,17 @@ class _NewItemState extends State<NewItem> {
           'category': _selectedCategory.title,
         }),
       );
+      // .then((response) {
+      //   //add the response of the post
+      // });
+
+      print(response.body);
+      print(response.statusCode);
+
+      if(!context.mounted) {
+        return;
+      }
+      Navigator.of(context).pop();
     }
   }
 
