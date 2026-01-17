60a8b1263a1b1186cfddf37a432c94cf65b035b7
johnatan <johnatanv91@gmail.com>
2026-01-17 13:19:41 -0500

    unnecessary import

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shopping_list_app/data/categories.dart';
-import 'package:shopping_list_app/models/category.dart';
 import 'dart:convert';
 import 'package:shopping_list_app/models/grocery_item.dart';
 import 'package:shopping_list_app/widgets/new_item.dart';
