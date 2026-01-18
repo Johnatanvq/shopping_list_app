0429ec1c3508e8264ddfb43444790a32fe734327
johnatan <johnatanv91@gmail.com>
2026-01-18 15:57:48 -0500

    sending delete request and flag for error for requests

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -72,12 +72,20 @@ class _GroceryListState extends State<GroceryList> {
     });
   }
 
-  void _removeItem(GroceryItem item) {
+  void _removeItem(GroceryItem item) async {
     final url = Uri.https('shopping-list-app-124ad-default-rtdb.firebaseio.com', 'shopping-list/${item.id}.json');
-
+    final index = _groceryItems.indexOf(item);
     setState(() {
       _groceryItems.remove(item);
     });
+
+    final response = await http.delete(url);
+    if (response.statusCode >= 400) {
+      //optional: show error
+      setState(() {
+        _groceryItems.insert(index, item);
+      });
+    }
   }
 
   @override
